f7b09683942142d8375c1a8c7f4ac7fe85acb683
ABDULLATIFYOUSUF <latifyousuf513@gmail.com>
2025-04-15 19:14:17 +0500

    frontend updated

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -6,27 +6,24 @@ import { BASE_URI } from "../config";
 function Navbar() {
   const [name, setName] = useState("");
   const [image, setImage] = useState("");
-  const login = JSON.parse(localStorage.getItem("user"));;
+  const login = JSON.parse(localStorage.getItem("user"));
   const navigate = useNavigate();
   const logoutBtn = () => {
     localStorage.clear();
     navigate("/");
   };
   const getUserById = async () => {
-    
-      if(login){
-        try {
-        const { data } = await axios.get(
-          `${BASE_URI}/api/v1/user/get_user/${login}`
-        );
-        if (data.success) {
-          setName(data.user.username);
-          setImage(data.user.image)
-        }
-      } catch (error) {
-        console.log(error);
+    try {
+      const { data } = await axios.get(
+        `${BASE_URI}/api/v1/user/get_user/${login}`
+      );
+      if (data.success) {
+        setName(data.user.username);
+        setImage(data.user.image);
       }
-      }
+    } catch (error) {
+      console.log(error);
+    }
   };
   useEffect(() => {
     getUserById();
@@ -36,7 +33,9 @@ function Navbar() {
       <div className="flex justify-between items-center bg-blue-600 text-white font-mono p-4">
         <div>
           {!login ? (
-            <a href="/" className="text-xl font-bold">Blog App</a>
+            <a href="/" className="text-xl font-bold">
+              Blog App
+            </a>
           ) : (
             <div className="flex items-center gap-4 text-xl font-bold">
               <img className="w-10 h-10 rounded-full" src={image} alt="" />
@@ -45,17 +44,19 @@ function Navbar() {
           )}
         </div>
         <div>
-          {login && <ul className="flex gap-4">
-            <li className="hover:underline hover:transition hover:ease-in-out hover: text-xl">
-              <a href="/">Home</a>
-            </li>
-            <li className="hover:underline hover:transition hover:ease-in-out hover: text-xl">
-              <a href="/create_blogs">Create Blogs</a>
-            </li>
-            <li className="hover:underline hover:transition hover:ease-in-out hover: text-xl">
-              <a href="/my_blogs">My Blogs</a>
-            </li>
-          </ul>}
+          {login && (
+            <ul className="flex gap-4">
+              <li className="hover:underline hover:transition hover:ease-in-out hover: text-xl">
+                <a href="/">Home</a>
+              </li>
+              <li className="hover:underline hover:transition hover:ease-in-out hover: text-xl">
+                <a href="/create_blogs">Create Blogs</a>
+              </li>
+              <li className="hover:underline hover:transition hover:ease-in-out hover: text-xl">
+                <a href="/my_blogs">My Blogs</a>
+              </li>
+            </ul>
+          )}
         </div>
         <div>
           <h1>
